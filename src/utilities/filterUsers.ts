fe49b3e3f22c5cf15e5ab4561f62ade0aedb8f4c
alinapavlyuk <alino4kap12@gmail.com>
2024-09-05 22:41:55 +0900

    Made filtering by substring

--- src/utilities/filterUsers.ts
+++ src/utilities/filterUsers.ts
@@ -2,22 +2,20 @@ import IUser from "../types/IUser.ts";
 import IFilters from "../types/IFilters.ts";
 
 export function filterUsers(users: IUser[], filters: IFilters) {
-  return users.filter((user: IUser) =>
-    Object.entries(filters).every(([key, value]): boolean => {
-      if (value === "") return true;
+    return users.filter((user: IUser) =>
+        Object.entries(filters).every(([key, value]): boolean => {
+            if (value === "") return true;
 
-      const userValue = user[key as keyof IUser];
+            const userValue = user[key as keyof IUser];
 
-      const normalizedUserValue =
-        typeof userValue === "number"
-          ? userValue.toString().toLowerCase().replace(/-/g, "")
-          : (userValue as string).toLowerCase().replace(/-/g, "");
+            const normalizedUserValue =
+                typeof userValue === "number"
+                    ? userValue.toString().trim().toLowerCase().replace(/-/g, "")
+                    : (userValue as string).trim().toLowerCase().replace(/-/g, "");
 
-      const normalizedFilterValue: string = value
-        .toLowerCase()
-        .replace(/-/g, "");
+            const normalizedFilterValue: string = value.trim().toLowerCase().replace(/-/g, "");
 
-      return normalizedUserValue.startsWith(normalizedFilterValue);
-    }),
-  );
+            return normalizedUserValue.includes(normalizedFilterValue);
+        }),
+    );
 }
