a7a0ba667f12dac743a575e5f6c79d5f90be976e
alinapavlyuk <alino4kap12@gmail.com>
2024-09-05 18:53:21 +0900

    Add dist folder for GitHub Pages deployment

--- src/store/index.ts
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import usersReducer from "./usersSlice.ts";
-
-export const store = configureStore({
-  reducer: usersReducer,
-});
-
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
--- src/types/IFilters.ts
+++
@@ -1,7 +0,0 @@
-export default interface IFilters {
-  [key: string]: string;
-  name: string;
-  username: string;
-  email: string;
-  phone: string;
-}
